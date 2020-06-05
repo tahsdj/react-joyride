e3e49a3fc64b3bf6ab1daf33456193298d5ac343
Paul Tsai <tahsdj@gmail.com>
2020-06-05 23:55:07 +0800

    allow add multiple targets or targets as group

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -38,7 +38,12 @@ export default class JoyrideOverlay extends React.Component {
     spotlightPadding: PropTypes.number,
     styles: PropTypes.object.isRequired,
     target: PropTypes.oneOfType([PropTypes.object, PropTypes.string]).isRequired,
+    group: PropTypes.bool
   };
+
+  static defaultProps = {
+    group: false
+  }
 
   componentDidMount() {
     const { debug, disableScrolling, disableScrollParentFix, target } = this.props;
@@ -99,6 +104,45 @@ export default class JoyrideOverlay extends React.Component {
     this.scrollParent.removeEventListener('scroll', this.handleScroll);
   }
 
+  get getGroupStyles(elements) {
+    const { showSpotlight } = this.state;
+    const {
+      spotlightClicks,
+      spotlightPadding,
+      styles,
+    } = this.props;
+
+    const elementsSortedByPosY = [...elements].sort((el1, el2) => {
+      return el1.getBoundingClientRect().y - el2.getBoundingClientRect().y
+    })
+    const elementsSortedByPosX = [...elements].sort((el1, el2) => {
+        return el1.getBoundingClientRect().x - el2.getBoundingClientRect().x
+    })
+
+    const topElementPos = elementsSortedByPosY[0].getBoundingClientRect()
+    const bottomElementPos = elementsSortedByPosY[elementsSortedByPosY.length - 1].getBoundingClientRect()
+
+    const leftElementPos = elementsSortedByPosX[0].getBoundingClientRect()
+    const rightElementPos = elementsSortedByPosX[elementsSortedByPosX.length - 1].getBoundingClientRect()
+
+    const height = bottomElementPos.y - topElementPos.y + bottomElementPos.height + spotlightPadding * 2
+    const width = rightElementPos.x - leftElementPos.x + rightElementPos.width + spotlightPadding * 2
+
+    const isFixedTarget = hasPosition(elements[0]);
+    
+    return {
+      ...(isLegacy() ? styles.spotlightLegacy : styles.spotlight),
+      height: Math.round(height),
+      left: Math.round(leftElementPos.x - spotlightPadding),
+      opacity: showSpotlight ? 1 : 0,
+      pointerEvents: spotlightClicks ? 'none' : 'auto',
+      position: isFixedTarget ? 'fixed' : 'absolute',
+      top: topElementPos.y - spotlightPadding,
+      transition: 'opacity 0.2s',
+      width: Math.round(width),
+    };
+  }
+
   get spotlightStyles() {
     const { showSpotlight } = this.state;
     const {
@@ -107,36 +151,57 @@ export default class JoyrideOverlay extends React.Component {
       spotlightPadding,
       styles,
       target,
+      group
     } = this.props;
-    const element = getElement(target);
-    const elementRect = getClientRect(element);
-    const isFixedTarget = hasPosition(element);
-    const top = getElementPosition(element, spotlightPadding, disableScrollParentFix);
 
-    return {
-      ...(isLegacy() ? styles.spotlightLegacy : styles.spotlight),
-      height: Math.round(elementRect.height + spotlightPadding * 2),
-      left: Math.round(elementRect.left - spotlightPadding),
-      opacity: showSpotlight ? 1 : 0,
-      pointerEvents: spotlightClicks ? 'none' : 'auto',
-      position: isFixedTarget ? 'fixed' : 'absolute',
-      top,
-      transition: 'opacity 0.2s',
-      width: Math.round(elementRect.width + spotlightPadding * 2),
-    };
+    const elements = (()=>{
+      if (typeof target === 'string') return [
+        ...document.querySelectorAll(target)
+      ]
+      return target && target.length > 0 ? [...target] : [target]
+    })()
+
+    if (!group || elements.length < 2) {
+      return elements.map(element => {
+        const elementRect = getClientRect(element);
+        const isFixedTarget = hasPosition(element);
+        const top = getElementPosition(element, spotlightPadding, disableScrollParentFix);
+
+        return {
+          ...(isLegacy() ? styles.spotlightLegacy : styles.spotlight),
+          height: Math.round(elementRect.height + spotlightPadding * 2),
+          left: Math.round(elementRect.left - spotlightPadding),
+          opacity: showSpotlight ? 1 : 0,
+          pointerEvents: spotlightClicks ? 'none' : 'auto',
+          position: isFixedTarget ? 'fixed' : 'absolute',
+          top,
+          transition: 'opacity 0.2s',
+          width: Math.round(elementRect.width + spotlightPadding * 2),
+        };
+      })
+    }
+
+    return this.getGroupStyles(elements)
   }
 
   handleMouseMove = e => {
     const { mouseOverSpotlight } = this.state;
-    const { height, left, position, top, width } = this.spotlightStyles;
 
-    const offsetY = position === 'fixed' ? e.clientY : e.pageY;
-    const offsetX = position === 'fixed' ? e.clientX : e.pageX;
-    const inSpotlightHeight = offsetY >= top && offsetY <= top + height;
-    const inSpotlightWidth = offsetX >= left && offsetX <= left + width;
-    const inSpotlight = inSpotlightWidth && inSpotlightHeight;
+    const isInAnySpotLight = this.spotlightStyles.some(spotlightStyle => {
 
-    if (inSpotlight !== mouseOverSpotlight) {
+      const { height, left, position, top, width } = spotlightStyle;
+
+      const offsetY = position === 'fixed' ? e.clientY : e.pageY;
+      const offsetX = position === 'fixed' ? e.clientX : e.pageX;
+      const inSpotlightHeight = offsetY >= top && offsetY <= top + height;
+      const inSpotlightWidth = offsetX >= left && offsetX <= left + width;
+      const inSpotlight = inSpotlightWidth && inSpotlightHeight;
+
+      return inSpotlight
+
+    })
+
+    if (isInAnySpotLight !== mouseOverSpotlight) {
       this.updateState({ mouseOverSpotlight: inSpotlight });
     }
   };
@@ -211,21 +276,30 @@ export default class JoyrideOverlay extends React.Component {
       ...baseStyles,
     };
 
-    let spotlight = placement !== 'center' && showSpotlight && (
-      <Spotlight styles={this.spotlightStyles} />
-    );
+    const spotlights = this.spotlightStyles.map((spotlightStyles, index) => {
 
-    // Hack for Safari bug with mix-blend-mode with z-index
-    if (getBrowser() === 'safari') {
-      const { mixBlendMode, zIndex, ...safarOverlay } = stylesOverlay;
+      let spotlight = placement !== 'center' && showSpotlight && (
+        <Spotlight styles={spotlightStyles} key={index}/>
+      );
 
-      spotlight = <div style={{ ...safarOverlay }}>{spotlight}</div>;
-      delete stylesOverlay.backgroundColor;
-    }
+      // Hack for Safari bug with mix-blend-mode with z-index
+      if (getBrowser() === 'safari') {
+        const { mixBlendMode, zIndex, ...safarOverlay } = stylesOverlay;
+
+        spotlight = (
+          <div style={{ ...safarOverlay }} key={index}>
+            {spotlight}
+          </div>
+        );
+        delete stylesOverlay.backgroundColor;
+      }
+
+      return spotlight
+    })
 
     return (
       <div className="react-joyride__overlay" style={stylesOverlay} onClick={onClickOverlay}>
-        {spotlight}
+        {spotlights}
       </div>
     );
   }
